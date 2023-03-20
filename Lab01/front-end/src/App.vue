787f4fa0c3cf75bf29987dcde8b628895ee095e1
chinhquoc01 <qhanpuoc01@gmail.com>
2023-03-20 23:29:28 +0700

    lab1

---
+++ Lab01/front-end/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="DevOps Lab" :weatherList="weatherList"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      weatherList: []
+    }
+  },
+  async beforeMount() {
+    let data = await fetch('http://server/WeatherForecast')
+    this.weatherList = await data.json()
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Lab01/front-end/src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div style="margin-top: 10px;" v-for="weather in weatherList" :key="weather.date">
+      <div class="flex-row">
+        <div>Date: </div>
+        <div>{{ weather.date }}</div>
+      </div>
+      <div class="flex-row">
+        <div>Temperature: </div>
+        <div>{{ weather.temperatureC }} &#8451;</div>
+      </div>
+      <div class="flex-row">
+        <div>Summary: </div>
+        <div>{{ weather.summary }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    weatherList: Array
+  },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.flex-row {
+  display: flex;
+}
+.flex-column {
+  display: flex;
+  flex-direction: column;
+}
+</style>
