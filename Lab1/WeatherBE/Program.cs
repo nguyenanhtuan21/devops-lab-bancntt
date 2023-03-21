607447a034d8f81a7a23c200dcd1e6e67a83a2b6
MrSunfat <trannhatdanh15062001gmail.com>
2023-03-21 22:50:02 +0700

    lab01

--- Lab1/WeatherBE/Program.cs
+++ Lab1/WeatherBE/Program.cs
@@ -1,23 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
-var myAllowSpecificOrigins = "AllowSpecificOrigins";
+var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 // enable cors
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: myAllowSpecificOrigins, policy =>
     {
-        policy.WithOrigins("http://localhost:8088")
+        policy.WithOrigins("http://localhost:8080", "http://localhost:8088")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
 });
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -27,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
