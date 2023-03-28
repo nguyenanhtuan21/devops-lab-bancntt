5f64c82b3ba6537d9da0cdd3b6ed633abb90284f
chinhquoc01 <qhanpuoc01@gmail.com>
2023-03-28 23:07:15 +0700

    lab 02

---
+++ Lab02/client/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to DevOps Lab 2"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  async mounted() {
+    try {
+      let data = await fetch('https://localhost/')
+      let res = await data.json()
+      console.log(res);
+      
+    } catch (error) {
+      let data = await fetch('http://localhost/')
+      let res = await data.json()
+      console.log(res);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
